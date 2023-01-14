bc1638522ecea4d582982c0399cf63795a3e1b52
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-01-14 15:28:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ the url it will send you 2 links which you can use to track people.
 \n1. Cloudflare Link: This method will show a cloudflare under attack page to gather informations and afterwards victim will be redirected to destinationed URL.
 \n2. Webview Link: This will show a website (ex bing , dating sites etc) using iframe for gathering information.
 ( ⚠️ Many sites may not work under this method if they have x-frame header present.Ex https://google.com )
-\n\nJoin Telegram Channel: https://t.me/OnlineHacKing
+\n\nJoin Telegram Channel: https://t.me/Heck_Tech
 `);
 }
   
